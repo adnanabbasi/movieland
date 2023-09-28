e8614fba00a60ee448fefca0da3ca9a897bda25f
adnanabbasi <adnan_abbasi@yahoo.com>
2023-09-27 20:49:58 -0400

    Changed the http to https

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard'
 import './App.css'
 import SearchIcon from './search.svg'
 
-const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_APIKEY} `
+const API_URL = `https://www.omdbapi.com?apikey=${process.env.REACT_APP_APIKEY} `
 
 const App = () => {
   const [movies, setMovies] = useState([])
